6e7c5293f238985622c16151da6bc471031591ef
Wenyao <peterleewenyaoalpha@gmail.com>
2020-04-09 00:18:22 +0800

    add constants for action types for consistency

--- client/src/context/GlobalReducer.ts
+++ client/src/context/GlobalReducer.ts
@@ -1,21 +1,22 @@
 import { GlobalStateProps } from './GlobalState';
 import { Action } from './GlobalAction';
 import { GetJobsResponse } from '../modules/Jobs/interface';
+import { SET_LOADING, SEARCH_JOBS, JOBS_ERROR } from './constants';
 
 const reducer = (state: GlobalStateProps, action: Action): GlobalStateProps => {
     switch (action.type) {
-        case 'SET_LOADING':
+        case SET_LOADING:
             return {
                 ...state,
                 isLoading: action.payload as GlobalStateProps['isLoading'],
             };
-        case 'SEARCH_JOBS':
+        case SEARCH_JOBS:
             return {
                 ...state,
                 isLoading: false,
                 data: action.payload as GetJobsResponse,
             };
-        case 'JOBS_ERROR':
+        case JOBS_ERROR:
             return {
                 ...state,
                 isLoading: false,
