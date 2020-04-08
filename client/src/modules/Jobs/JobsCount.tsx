b8b6b8f35abcc1ff96286febfc1b4351fc01b1e1
Wenyao <peterleewenyaoalpha@gmail.com>
2020-04-08 22:56:08 +0800

    add loading condition to render content accordingly

--- client/src/modules/Jobs/JobsCount.tsx
+++ client/src/modules/Jobs/JobsCount.tsx
@@ -2,11 +2,11 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../../context/GlobalState';
 
 const JobsCount: React.FC = () => {
-    const { data } = useContext(GlobalContext);
+    const { data, isLoading } = useContext(GlobalContext);
 
     const { size } = data;
 
-    return <>{!!size && <div>{size} jobs found</div>}</>;
+    return <>{!isLoading && !!size && <div>{size} jobs found</div>}</>;
 };
 
 export default JobsCount;
